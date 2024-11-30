b6a6bb68d62e9179f8c7fdc6eb1d856b0bf4aa4f
Rafael Tomé de Souza <rafaeltomesouza@gmail.com>
2024-11-30 01:07:52 -0300

    Removed tests

--- tests/unit/controllers/versao.test.js
+++ tests/unit/controllers/versao.test.js
@@ -44,27 +44,4 @@ describe('Versao Controller', () => {
     expect(res.send).toHaveBeenCalledWith('Bia 1.0.0');
   });
 
-  test('get deve retornar a string de resposta correta quando VERSAO_API está definido', () => {
-    // Simula o cenário onde VERSAO_API está definido
-    process.env.VERSAO_API = '1.0.0';
-
-    // Chama a função retornada pelo controller para obter o objeto controller
-    const { get } = versaoController();
-    // Chama o método get do objeto controller
-    get(req, res);
-
-    expect(res.send).toHaveBeenCalledWith('Bia 1.0.0');
-  });
-
-  test('get deve retornar a string de resposta correta quando VERSAO_API está definido', () => {
-    // Simula o cenário onde VERSAO_API está definido
-    process.env.VERSAO_API = '1.0.0';
-
-    // Chama a função retornada pelo controller para obter o objeto controller
-    const { get } = versaoController();
-    // Chama o método get do objeto controller
-    get(req, res);
-
-    expect(res.send).toHaveBeenCalledWith('Bia 1.0.0');
-  });  
 });
